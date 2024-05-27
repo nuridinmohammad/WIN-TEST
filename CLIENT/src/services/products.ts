29b32588bb69811d146a2eb312bf7243a4e589bf
mohammadnuridin <75136633+nuridinmohammad@users.noreply.github.com>
2024-05-28 00:02:56 +0700

    Update products.ts

--- CLIENT/src/services/products.ts
+++ CLIENT/src/services/products.ts
@@ -5,8 +5,8 @@ import { Cookies } from "react-cookie";
 export const productsApi = createApi({
   reducerPath: "productsApi",
   baseQuery: fetchBaseQuery({
-    // baseUrl: "https://win-server-7ar9pzc1b-nuridinmohammads-projects.vercel.app/api",
-    baseUrl: "http://localhost:5000/api",
+    baseUrl: "https://win-server.vercel.app/api",
+    // baseUrl: "http://localhost:5000/api",
 
     prepareHeaders: (headers) => {
       const token = new Cookies().get("access_token");
