5fdfc6742533294c703f23b8ef181abf6873c7e0
mohammadnuridin <75136633+nuridinmohammad@users.noreply.github.com>
2024-05-28 00:02:30 +0700

    Update auth.ts

--- CLIENT/src/services/auth.ts
+++ CLIENT/src/services/auth.ts
@@ -13,8 +13,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    // baseUrl: "https://win-server-7ar9pzc1b-nuridinmohammads-projects.vercel.app/api",
-    baseUrl: "http://localhost:5000/api",
+    baseUrl: "https://win-server.vercel.app/api",
+    // baseUrl: "http://localhost:5000/api",
   }),
   tagTypes: ["auth"],
   endpoints: (builder) => ({
